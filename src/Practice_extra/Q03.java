04c38a299aa62a166b2a06ab0b4ca9cdd047119e
Kubra Ozyasar <kubraozyasar.35@gmail.com>
2022-05-08 08:25:13 -0600

    Commiting new class

---
+++ src/Practice_extra/Q03.java
@@ -0,0 +1,34 @@
+package Practice_extra;
+
+import java.util.Arrays;
+
+public class Q03 {
+    public static void main(String[] args) {
+
+        /*
+         * create a method accepts a name as parameter
+         * and prints the name as a char array
+         *
+         * (do not use toCharArray() method)
+         *
+         * Input : John
+         * Output : [J, o, h, n]
+         *
+         */
+
+        //Call the method
+        charArray("Kubra");
+    }
+
+    public static void charArray(String str) {   // created method accepts a name as parameter
+
+        char array[] = new char[str.length()];//
+
+        for (int i =0; i<str.length(); i++){
+
+            array[i]=str.charAt(i);
+        }
+        System.out.println(Arrays.toString(array));
+
+    }
+}
